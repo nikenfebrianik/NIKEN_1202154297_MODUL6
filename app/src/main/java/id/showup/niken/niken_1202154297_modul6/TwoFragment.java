ff9a0f07234843b2a2bfd0e3820192d59a40f72c
Niken Febriani Kusumawati <nikenfebrianik@gmail.com>
2018-04-01 22:06:33 +0700

    comment ajah

--- app/src/main/java/id/showup/niken/niken_1202154297_modul6/TwoFragment.java
+++ app/src/main/java/id/showup/niken/niken_1202154297_modul6/TwoFragment.java
@@ -1,7 +1,5 @@
 package id.showup.niken.niken_1202154297_modul6;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
